b57a35984251a6fb7f9b5652b6e09145c73838ad
Adrian Fitzpatrick <adrianjamesfitzpatrick@gmail.com>
2013-11-13 22:18:28 +1100

    Added comments

--- NodeJS/whispers.js
+++ NodeJS/whispers.js
@@ -1,3 +1,9 @@
+/**
+* Author: Adrian Fitzpatrick
+* This NodeJS script is based on the children's game Chines Whispers.  A sentence is passed in to the script on the command line,
+* the script then randomly selects words from the sentence and swaps them for a similar sounding word.
+* Acknowledgement to Rhymebrain.com for the service used to get a set of rhyming words.
+*/
 var url = "http://rhymebrain.com/talk?function=getRhymes&maxResults=10&word="
 var http = require("http")
 var MAX = 10;
